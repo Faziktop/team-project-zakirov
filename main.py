8b5b41cdaa472117f899cf87a39741267a59625b
faziktop <duhguhg564@gmail.com>
2026-01-13 19:48:47 +0400

    Add divide function\

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
 def main():
     print("Hello, Team!")
+    result = divide(10, 2)
+    print(f"Division result: {result}")
 
 if __name__ == "__main__":
     main()
